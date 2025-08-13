4571e13b371da92976af3dc95b0629deaa3d7a93
Warren Brown <warrengbrn@gmail.com>
2025-08-13 19:07:16 -0400

    updated yml

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,5 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => ({
-    base: '/d3js-geoprojection',
-    build: {
-        rollupOptions: {
-            // Ensure d3 and other deps are NOT externalized
-            external: (id) => {
-                // Return false to bundle everything
-                return false;
-            }
-        }
-    },
-    optimizeDeps: {
-        include: ['html-to-image', 'd3']
-    }
+export default defineConfig(() => ({
+    base: '/d3js-geoprojection/',
 }));
